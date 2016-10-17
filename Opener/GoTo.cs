c701ae7bc03844dbe72403ba229a1047126dfc46
Valerii Lashmanov <vflashm@gmail.com>
2016-10-17 10:21:06 -0400

    text under cursor impl

--- Opener/GoTo.cs
+++ Opener/GoTo.cs
@@ -10,28 +10,49 @@ namespace Opener
 {
     class GoTo
     {
+        static bool IsId(char c)
+        {
+            return Char.IsLetter(c) 
+                || Char.IsDigit(c) 
+                || c == '.' 
+                || c == '_'
+                || c == '@'
+                || c == '#'
+                || c == '$';
+        }
+
         static string WordUnderCursor(DTE2 application)
         {
             Document doc = application.ActiveDocument;
-            if (doc == null) {
+            if (doc == null) 
+            {
                 return null;
             }
             TextDocument textDoc = doc.Object("TextDocument") as TextDocument;
-
-            var ep = textDoc.StartPoint.CreateEditPoint();
-            string s = ep.GetText(textDoc.EndPoint);
-            int offset = textDoc.Selection.ActivePoint.AbsoluteCharOffset;
-            while (offset > 0 && (Char.IsLetter(s[offset-1]) || Char.IsDigit(s[offset-1])))
+            if (textDoc == null)
             {
-                offset -= 1;
+                return null;
             }
-            int to = offset;
-            while (to < s.Length && (Char.IsLetter(s[to+1]) || Char.IsDigit(s[to+1])))
+
+            var editPoint = textDoc.StartPoint.CreateEditPoint();
+            string text = editPoint.GetText(textDoc.EndPoint);
+            text = text.Replace("\r\n", "\n");
+
+            int from = textDoc.Selection.ActivePoint.AbsoluteCharOffset - 1;
+            while (from > 0 && IsId(text[from - 1]))
+            {
+                from -= 1;
+            }
+            int to = from;
+            while (to < text.Length && IsId(text[to]))
             {
                 to += 1;
             }
-            string ssss = s.Substring(offset, to - offset + 1);
-            return ssss;
+            if (from == to)
+            {
+                return null;
+            }
+            return text.Substring(from, to - from);
         }
 
         static public bool Execute(DTE2 application)
