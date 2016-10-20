4d54e8da83fd30b4b20474a8be8633f0e6709864
Valerii Lashmanov <vflashm@gmail.com>
2016-10-20 15:43:06 -0400

    joiner barebones

---
+++ Joiner/Joiner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EnvDTE;
+using EnvDTE80;
+
+namespace Joiner
+{
+    class Joiner
+    {
+        static public bool Execute(DTE2 application)
+        {
+            Document doc = application.ActiveDocument;
+            if (doc == null)
+            {
+                return false;
+            }
+            TextDocument textDoc = doc.Object("TextDocument") as TextDocument;
+            if (textDoc == null)
+            {
+                return false;
+            }
+
+            var editPoint = textDoc.StartPoint.CreateEditPoint();
+            string text = editPoint.GetText(textDoc.EndPoint);
+            text = text.Replace("\r\n", "\n");
+
+            return false;
+        }
+    }
+}
