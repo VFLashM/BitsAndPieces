67546000aa5cfed1b454df3572ac107c9ac5a456
Valerii Lashmanov <vflashm@gmail.com>
2016-10-20 17:18:33 -0400

    simple regex-based context parser

---
+++ Joiner/ContextInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Joiner
+{
+    class ContextInfo
+    {
+        List<TableInfo> joinedTables;
+        TableInfo newTable;
+        bool hasGlue;
+
+        public ContextInfo(List<TableInfo> joinedTables, TableInfo newTable, bool hasGlue)
+        {
+            this.joinedTables = joinedTables;
+            this.newTable = newTable;
+            this.hasGlue = hasGlue;
+        }
+    }
+}
---
+++ Joiner/TableInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Joiner
+{
+    class TableInfo
+    {
+        private List<string> id;
+        private string alias;
+
+        public TableInfo(List<string> id, string alias)
+        {
+            this.id = id;
+            this.alias = alias;
+        }
+        
+    }
+}
