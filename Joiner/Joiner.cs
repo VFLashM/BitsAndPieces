d2bbff83130897c1286f914a15e6c44c68ec485e
Valerii Lashmanov <vflashm@gmail.com>
2016-10-20 17:23:15 -0400

    parser extracted

--- Joiner/Joiner.cs
+++ Joiner/Joiner.cs
@@ -10,95 +10,17 @@ namespace Joiner
 {
     class Joiner
     {
-        static readonly Regex fromRegex = new Regex(@"\bfrom\b\s*", RegexOptions.RightToLeft | RegexOptions.IgnoreCase);
-        static readonly Regex joinRegex = new Regex(@"\bjoin\b\s*", RegexOptions.IgnoreCase);
-        static readonly Regex identifierRegex = new Regex("^(" + String.Join("|", new string[] {
-            @"\[(?<id>[^\]]+)\]", // brackets identifier
-            @"""(?<id>[^""]+)""", // quoted identifier
-            @"(?<id>[a-zA-Z_@#][a-zA-Z_@#$0-9]*)", // regular identifier
-        }) + ")");
-        static readonly Regex aliasRegex = new Regex(@"^\s+(?<as>as\s+)?(?<alias>[a-zA-Z_][a-zA-Z_0-9]*)", RegexOptions.IgnoreCase);
-        static readonly Regex onRegex = new Regex(@"\bon\b\s*", RegexOptions.IgnoreCase);
-        static readonly Regex whitespaceRegex = new Regex(@"^\s*$");
-
-        static bool ConsumeRegex(ref string str, Regex regex, out Match match)
-        {
-            match = regex.Match(str);
-            if (match.Success)
-            {
-                str = str.Substring(match.Index + match.Groups[0].Length);
-            }
-            return match.Success;
-        }
-
-        static bool ConsumeId(ref string str, out List<String> id)
-        {
-            id = new List<string>();
-
-            while (true)
-            {
-                Match match;
-                if (ConsumeRegex(ref str, identifierRegex, out match))
-                {
-                    id.Add(match.Groups["id"].Value);
-                    if (str.StartsWith("."))
-                    {
-                        str = str.Substring(1);
-                    }
-                    else
-                    {
-                        return true;
-                    }
-                }
-                else if (str.StartsWith("."))
-                {
-                    id.Add(null);
-                    str = str.Substring(1);
-                }
-                else
-                {
-                    return false;
-                }
-            }
-        }
-
-        static bool ConsumeTable(ref string str, out TableInfo table)
-        {
-            List<string> id;
-            if (!ConsumeId(ref str, out id))
-            {
-                table = null;
-                return false;
-            }
-
-            Match match;
-            string alias = null;
-            string beforeAliasStr = str;
-            if (ConsumeRegex(ref str, aliasRegex, out match))
-            {
-                alias = match.Groups["alias"].Value;
-                if (!match.Groups["as"].Success && (alias.ToLower() == "join" || alias.ToLower() == "on"))
-                {
-                    str = beforeAliasStr;
-                    alias = null;
-                }
-            }
-
-            table = new TableInfo(id, alias);
-            return true;
-        }
-
-        static public ContextInfo Parse(DTE2 application)
+        static public bool Execute(DTE2 application)
         {
             Document doc = application.ActiveDocument;
             if (doc == null)
             {
-                return null;
+                return false;
             }
             TextDocument textDoc = doc.Object("TextDocument") as TextDocument;
             if (textDoc == null)
             {
-                return null;
+                return false;
             }
 
             var editPoint = textDoc.StartPoint.CreateEditPoint();
@@ -108,60 +30,7 @@ namespace Joiner
 
             string body = text.Substring(0, cursorPos);
 
-            Match match;
-            if (!ConsumeRegex(ref body, fromRegex, out match))
-            {
-                return null;
-            }
-
-            var tables = new List<TableInfo>();
-            TableInfo newTable;
-            if (!ConsumeTable(ref body, out newTable))
-            {
-                return null;
-            }
-            tables.Add(newTable);
-            if (whitespaceRegex.IsMatch(body))
-            {
-                return new ContextInfo(tables, null, false);
-            }
-            if (!ConsumeRegex(ref body, joinRegex, out match))
-            {
-                return null;
-            }
-
-            do
-            {
-                if (whitespaceRegex.IsMatch(body))
-                {
-                    return new ContextInfo(tables, null, true);
-                }
-                if (!ConsumeTable(ref body, out newTable))
-                {
-                    return null;
-                }
-                if (whitespaceRegex.IsMatch(body))
-                {
-                    return new ContextInfo(tables, newTable, false);
-                }
-                if (!ConsumeRegex(ref body, onRegex, out match))
-                {
-                    return null;
-                }
-                if (whitespaceRegex.IsMatch(body))
-                {
-                    return new ContextInfo(tables, newTable, true);
-                }
-                tables.Add(newTable);
-            }
-            while (ConsumeRegex(ref body, joinRegex, out match));
-
-            return new ContextInfo(tables, null, false);
-        }
-
-        static public bool Execute(DTE2 application)
-        {
-            ContextInfo context = Parse(application);
+            ContextInfo context = Parser.Parse(body);
             return context != null;
         }
     }
---
+++ Joiner/Parser.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Joiner
+{
+    class Parser
+    {
+        static readonly Regex fromRegex = new Regex(@"\bfrom\b\s*", RegexOptions.RightToLeft | RegexOptions.IgnoreCase);
+        static readonly Regex joinRegex = new Regex(@"\bjoin\b\s*", RegexOptions.IgnoreCase);
+        static readonly Regex identifierRegex = new Regex("^(" + String.Join("|", new string[] {
+            @"\[(?<id>[^\]]+)\]", // brackets identifier
+            @"""(?<id>[^""]+)""", // quoted identifier
+            @"(?<id>[a-zA-Z_@#][a-zA-Z_@#$0-9]*)", // regular identifier
+        }) + ")");
+        static readonly Regex aliasRegex = new Regex(@"^\s+(?<as>as\s+)?(?<alias>[a-zA-Z_][a-zA-Z_0-9]*)", RegexOptions.IgnoreCase);
+        static readonly Regex onRegex = new Regex(@"\bon\b\s*", RegexOptions.IgnoreCase);
+        static readonly Regex whitespaceRegex = new Regex(@"^\s*$");
+
+        static bool ConsumeRegex(ref string str, Regex regex, out Match match)
+        {
+            match = regex.Match(str);
+            if (match.Success)
+            {
+                str = str.Substring(match.Index + match.Groups[0].Length);
+            }
+            return match.Success;
+        }
+
+        static bool ConsumeId(ref string str, out List<String> id)
+        {
+            id = new List<string>();
+
+            while (true)
+            {
+                Match match;
+                if (ConsumeRegex(ref str, identifierRegex, out match))
+                {
+                    id.Add(match.Groups["id"].Value);
+                    if (str.StartsWith("."))
+                    {
+                        str = str.Substring(1);
+                    }
+                    else
+                    {
+                        return true;
+                    }
+                }
+                else if (str.StartsWith("."))
+                {
+                    id.Add(null);
+                    str = str.Substring(1);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        static bool ConsumeTable(ref string str, out TableInfo table)
+        {
+            List<string> id;
+            if (!ConsumeId(ref str, out id))
+            {
+                table = null;
+                return false;
+            }
+
+            Match match;
+            string alias = null;
+            string beforeAliasStr = str;
+            if (ConsumeRegex(ref str, aliasRegex, out match))
+            {
+                alias = match.Groups["alias"].Value;
+                if (!match.Groups["as"].Success && (alias.ToLower() == "join" || alias.ToLower() == "on"))
+                {
+                    str = beforeAliasStr;
+                    alias = null;
+                }
+            }
+
+            table = new TableInfo(id, alias);
+            return true;
+        }
+
+        static public ContextInfo Parse(string body)
+        {
+            Match match;
+            if (!ConsumeRegex(ref body, fromRegex, out match))
+            {
+                return null;
+            }
+
+            var tables = new List<TableInfo>();
+            TableInfo newTable;
+            if (!ConsumeTable(ref body, out newTable))
+            {
+                return null;
+            }
+            tables.Add(newTable);
+            if (whitespaceRegex.IsMatch(body))
+            {
+                return new ContextInfo(tables, null, false);
+            }
+            if (!ConsumeRegex(ref body, joinRegex, out match))
+            {
+                return null;
+            }
+
+            do
+            {
+                if (whitespaceRegex.IsMatch(body))
+                {
+                    return new ContextInfo(tables, null, true);
+                }
+                if (!ConsumeTable(ref body, out newTable))
+                {
+                    return null;
+                }
+                if (whitespaceRegex.IsMatch(body))
+                {
+                    return new ContextInfo(tables, newTable, false);
+                }
+                if (!ConsumeRegex(ref body, onRegex, out match))
+                {
+                    return null;
+                }
+                if (whitespaceRegex.IsMatch(body))
+                {
+                    return new ContextInfo(tables, newTable, true);
+                }
+                tables.Add(newTable);
+            }
+            while (ConsumeRegex(ref body, joinRegex, out match));
+
+            return new ContextInfo(tables, null, false);
+        }
+    }
+}
