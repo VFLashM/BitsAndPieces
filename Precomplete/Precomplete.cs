e8564585e928b2de6820e228b79b26533d90c49f
Valerii Lashmanov <vflashm@gmail.com>
2017-02-16 12:58:06 -0500

    precomplete actually works

--- Precomplete/Precomplete.cs
+++ Precomplete/Precomplete.cs
@@ -7,15 +7,21 @@ using Microsoft.SqlServer.Management.UI.VSIntegration;
 using Microsoft.SqlServer.Management.Smo.RegSvrEnum;
 using Microsoft.SqlServer.Management.Smo;
 using Microsoft.SqlServer.Management.Common;
+using System.Text.RegularExpressions;
 
 namespace Precomplete
 {
     class Precomplete
     {
+        static readonly Regex wordRegex = new Regex(@"[^.@]\b([a-zA-Z_@#][a-zA-Z_@#$0-9]*)\s*[(]?$", RegexOptions.RightToLeft | RegexOptions.IgnoreCase);
+        static readonly Regex fromJoinRegex = new Regex(@"(from|join)\s([a-zA-Z_@#][a-zA-Z_@#$0-9]*)\s*[(]?$", RegexOptions.RightToLeft | RegexOptions.IgnoreCase);
+        static readonly Regex execRegex = new Regex(@"\bexec\s([a-zA-Z_@#][a-zA-Z_@#$0-9]*)\s*$", RegexOptions.RightToLeft | RegexOptions.IgnoreCase);
+
         private class DatabaseInfo
         {
             public Dictionary<string, string> tables = new Dictionary<string, string>();
             public Dictionary<string, string> functions = new Dictionary<string, string>();
+            public Dictionary<string, string> procedures = new Dictionary<string, string>();
         }
         private class ServerInfo
         {
@@ -64,6 +70,10 @@ namespace Precomplete
                     {
                         databaseInfo.functions[function.Name] = function.Schema;
                     }
+                    foreach (StoredProcedure proc in database.StoredProcedures)
+                    {
+                        databaseInfo.procedures[proc.Name] = proc.Schema;
+                    }
                     res.databases[database.Name] = databaseInfo;
                 }
             }
@@ -86,6 +96,12 @@ namespace Precomplete
 
         public void Apply(TextDocument textDoc, TextPoint startPoint, TextPoint endPoint)
         {
+            string delta = startPoint.CreateEditPoint().GetText(endPoint);
+            if (!(delta == " " || delta == "\t" || delta == "\r\n" || delta == "("))
+            {
+                return;
+            }
+
             UIConnectionInfo uiConnectionInfo = ServiceCache.ScriptFactory.CurrentlyActiveWndConnectionInfo.UIConnectionInfo;
             ServerInfo serverInfo = GetServerInfo(uiConnectionInfo);
             if (serverInfo == null)
@@ -94,12 +110,79 @@ namespace Precomplete
             }
 
             string text = textDoc.StartPoint.CreateEditPoint().GetText(endPoint);
-            /*
-            var editPoint = textDoc.StartPoint.CreateEditPoint();
-            string text = editPoint.GetText(textDoc.EndPoint);
             text = text.Replace(Environment.NewLine, "\n");
             int cursorPos = textDoc.Selection.ActivePoint.AbsoluteCharOffset - 1;
-             */
+            string activeDatabase = Common.Connection.GetActiveDatabase(text, cursorPos);
+
+            var wordMatch = wordRegex.Match(text);
+            if (!wordMatch.Success)
+            {
+                return;
+            }
+            var wordGroup = wordMatch.Groups[1];
+            string word = wordGroup.Value;
+
+            string prependDb = null;
+            string prependSchema = null;
+            if (fromJoinRegex.Match(text).Success)
+            {
+                foreach (var db in serverInfo.databases)
+                {
+                    if (db.Key != activeDatabase)
+                    {
+                        if (db.Value.tables.ContainsKey(word))
+                        {
+                            prependDb = db.Key;
+                            prependSchema = db.Value.tables[word];
+                            if (prependSchema == "dbo") prependSchema = "";
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (execRegex.Match(text).Success)
+            {
+                foreach (var db in serverInfo.databases)
+                {
+                    if (db.Key != activeDatabase)
+                    {
+                        if (db.Value.procedures.ContainsKey(word))
+                        {
+                            prependDb = db.Key;
+                            prependSchema = db.Value.procedures[word];
+                            if (prependSchema == "dbo") prependSchema = "";
+                            break;
+                        }
+                    }
+                }
+            }
+            else if (delta == "(")
+            {
+                foreach (var db in serverInfo.databases)
+                {
+                    if (db.Value.functions.ContainsKey(word))
+                    {
+                        prependDb = db.Key;
+                        prependSchema = db.Value.functions[word];
+                        break;
+                    }
+                }
+            }
+            string prependText = "";
+            if (prependDb != null)
+            {
+                prependText += prependDb + ".";
+            }
+            if (prependSchema != null)
+            {
+                prependText += prependSchema + ".";
+            }
+            if (prependText != "")
+            {
+                var editPoint = textDoc.CreateEditPoint();
+                editPoint.MoveToAbsoluteOffset(wordGroup.Index + 1);
+                editPoint.Insert(prependText);
+            }
         }
     }
 }
